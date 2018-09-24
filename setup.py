bd29e480237b2c7714f2b0fec46a23477b64973e
Simon Andermatt <simon.andermatt@unibas.ch>
2018-09-24 15:34:23 +0200

    adding optional requirements as must have, since at least one needs to be installed. if not installed via pip, not both torch and tensorflow need be installed.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author="Simon Andermatt",
     author_email="simon.andermatt@unibas.ch",
     url="https://github.com/zubata88/mdgru",
-    packages=find_packages('.') + ['__main__.py'],
+    packages=find_packages('.') + ['run'],
     license="LGPL",
     python_requires='>=3.5',
     install_requires=["nibabel", "numpy", "scipy", "pydicom", "matplotlib", "scikit-image", "tensorflow-gpu>=1.8", "torch", "torchvision"],
