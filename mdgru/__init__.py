65d61f03a74f8599ef43d6666b1d18b712450c09
Simon Andermatt <simon.andermatt@unibas.ch>
2018-09-24 15:09:08 +0200

    init script now imports from main module mdgru

---
+++ mdgru/__init__.py
@@ -0,0 +1,21 @@
+import mdgru.data
+import mdgru.runner
+import mdgru.helper
+import mdgru.eval
+try:
+    import mdgru.model
+except ModuleNotFoundError as e:
+    if e.name == "tensorflow":
+        print("Tensorflow is not installed")
+    else:
+        raise e
+try:
+    import mdgru.model_pytorch
+except ModuleNotFoundError as e:
+    if e.name == "torch":
+        print("Pytorch is not installed")
+    else:
+        raise e
+import mdgru.runner
+import mdgru.helper
+import mdgru.eval
