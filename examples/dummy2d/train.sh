be4bdfc2ee9459ea8e8f3a9327ac0c9571984164
Simon Andermatt <simon.andermatt@unibas.ch>
2018-10-02 17:03:40 +0200

    rebuilt docs

--- examples/dummy2d/train.sh
+++
@@ -1,2 +0,0 @@
-python3 ../../RUN_mdgru.py --datapath `pwd`/nifti --locationtraining train --locationvalidation val --locationtesting test --optionname discardme --modelname discardme -w 256 256 --paddingtesting 10 10 --windowsizetesting 256 256 -p 0 0 -f flair.nii.gz t2.nii.gz pd.nii.gz mprage.nii.gz -m mask1.nii.gz --iterations 100 --test_each 50 --nclasses 2 --num_threads 4
-
