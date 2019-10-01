9904c2d07be1deb176281efe16a3e5642a83f1ca
Georgi Tancev <30585013+gtancev@users.noreply.github.com>
2019-10-01 10:41:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url="https://github.com/gtancev/mdgru",
     packages=find_packages('.') + ['.'],
     license="LGPL",
-    python_requires='==3.5',
+    python_requires='>=3.5',
     install_requires=["nibabel==2.5.0", "numpy==1.15.1", "scipy==1.0.0", "pydicom==1.3.0", "matplotlib==3.0.3", "scikit-image==0.15.0", "simpleitk==1.2.2", "tensorflow-gpu==1.8", "torch==1.2.0", "torchvision==0.4.0", "visdom==0.1.8.9"],
     classifiers=[
         'Development Status :: 4 - Beta',
